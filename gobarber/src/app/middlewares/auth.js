a6ee68bf64b5cf8bb6894a4f59f8a4c187331232
Andre Oliveira <desmonplays@gmail.com>
2020-05-09 18:30:18 -0400

    (new feature) creating the middleware of authentication decoding the token

---
+++ gobarber/src/app/middlewares/auth.js
@@ -0,0 +1,24 @@
+import jwt from 'jsonwebtoken';
+import authConfig from '../../config/auth';
+import { promisify } from 'util';
+
+export default async (req, res, next) => {
+  const bearerToken = req.headers.authorization;
+
+  if (!bearerToken) {
+    return res.status(401).json({ error: 'Token not provided' });
+  }
+
+  const [, token] = bearerToken.split(' ');
+
+  try {
+    const decoded = await promisify(jwt.verify)(token, authConfig.secret);
+    req.userId = decoded.id;
+
+    return next();
+  } catch(err) {
+    return res.status(401).json({ message: 'Token invalid' });
+  }
+
+  return next();
+};
