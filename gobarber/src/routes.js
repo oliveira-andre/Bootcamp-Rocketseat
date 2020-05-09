9094f01bd693b63c8b06936bd6e7b652b5f12f3c
Andre Oliveira <desmonplays@gmail.com>
2020-05-09 18:29:57 -0400

    (new feature) importing and using the auth middleware and creating the put users route to update some fields of user

--- gobarber/src/routes.js
+++ gobarber/src/routes.js
@@ -2,9 +2,16 @@ import { Router } from 'express';
 import User from './app/models/User';
 
 import UsersController from './app/controllers/UsersController';
+import SessionsController from './app/controllers/SessionsController';
+
+import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
 
 routes.post('/users', UsersController.store);
+routes.post('/sessions', SessionsController.store);
+
+routes.use(authMiddleware);
+routes.put('/users', UsersController.update);
 
 export default routes;
