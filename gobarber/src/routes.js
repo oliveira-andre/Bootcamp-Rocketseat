430fe74c35db48c73405c9d0778bf7b6ca09e122
Andre Oliveira <desmonplays@gmail.com>
2020-05-10 21:58:23 -0400

    (new feature) importing files controller

--- gobarber/src/routes.js
+++ gobarber/src/routes.js
@@ -1,17 +1,23 @@
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
+
 import User from './app/models/User';
 
 import UsersController from './app/controllers/UsersController';
 import SessionsController from './app/controllers/SessionsController';
+import FilesController from './app/controllers/FilesController';
 
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.post('/users', UsersController.store);
 routes.post('/sessions', SessionsController.store);
 
 routes.use(authMiddleware);
 routes.put('/users', UsersController.update);
+routes.post('/files', upload.single('file'), FilesController.store);
 
 export default routes;
