20968163679544accf9b1790a1388bb1a19f2a2e
Andre Oliveira <desmonplays@gmail.com>
2020-05-10 23:10:04 -0400

    (new feature) reading all static method associate of all models

--- gobarber/src/database/index.js
+++ gobarber/src/database/index.js
@@ -2,8 +2,9 @@ import Sequelize from 'sequelize';
 import dbConfig from '../config/database';
 
 import User from '../app/models/User';
+import File from '../app/models/File';
 
-const models = [User];
+const models = [User, File];
 
 class Database {
   constructor() {
@@ -12,7 +13,8 @@ class Database {
 
   init() {
     this.connection = new Sequelize(dbConfig);
-    models.map((model) => model.init(this.connection));
+    models.map((model) => model.init(this.connection))
+    models.map((model) => model.associate && model.associate(this.connection.models));
   }
 }
 
