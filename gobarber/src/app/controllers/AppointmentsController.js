2ae754b4deac0ff597c0de64340046b59bfd9095
Andre Oliveira <desmonplays@gmail.com>
2020-05-23 17:01:35 -0400

    (new feature) cancelling and validating appointments

--- gobarber/src/app/controllers/AppointmentsController.js
+++ gobarber/src/app/controllers/AppointmentsController.js
@@ -3,7 +3,7 @@ import Appointment from '../models/Appointment';
 import User from '../models/User';
 import File from '../models/File';
 import * as Yup from 'yup';
-import { startOfHour, parseISO, isBefore, format } from 'date-fns';
+import { startOfHour, parseISO, isBefore, format, subHours } from 'date-fns';
 import pt from 'date-fns/locale/pt';
 
 class AppointmentsController {
@@ -87,6 +87,25 @@ class AppointmentsController {
     });
 
     return res.json(appointment);
+  }
+
+  async delete(req, res) {
+    const appointment = await Appointment.findByPk(req.params.id);
+
+    if(appointment.user_id !== req.userId) {
+      return res.status(401).json({ error: "You don't have permission to cancel this appointment" });
+    }
+
+    const dateWithSub = subHours(appointment.date, 2);
+
+    if(isBefore(dateWithSub, new Date())) {
+      return res.status(401).json({ error: 'you can only cancel appointments with 2 hours in advance' });
+    }
+
+    appointment.canceled_at = new Date();
+    await appointment.save();
+  
+    res.json(appointment);
   }
 }
 
