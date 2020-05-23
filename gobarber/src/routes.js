710965872eaac682dd789b0d3af27f6aa2ac59bb
Andre Oliveira <desmonplays@gmail.com>
2020-05-23 17:01:23 -0400

    (new feature) routing the read notification and the cancel of appointment

--- gobarber/src/routes.js
+++ gobarber/src/routes.js
@@ -10,6 +10,7 @@ import FilesController from './app/controllers/FilesController';
 import ProvidersController from './app/controllers/ProvidersController';
 import AppointmentsController from './app/controllers/AppointmentsController';
 import SchedulesController from './app/controllers/SchedulesController';
+import NotificationsController from './app/controllers/NotificationsController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -21,10 +22,18 @@ routes.post('/sessions', SessionsController.store);
 
 routes.use(authMiddleware);
 routes.put('/users', UsersController.update);
+
 routes.post('/files', upload.single('file'), FilesController.store);
+
 routes.get('/providers', ProvidersController.index);
+
 routes.get('/appointments', AppointmentsController.index);
 routes.post('/appointments', AppointmentsController.store);
+routes.delete('/appointments/:id', AppointmentsController.delete);
+
 routes.get('/schedules', SchedulesController.index);
+
+routes.get('/notifications', NotificationsController.index);
+routes.put('/notifications/:id', NotificationsController.update);
 
 export default routes;
