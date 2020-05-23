ece9743f2c380c49b569c1e12511a48751a722c7
Andre Oliveira <desmonplays@gmail.com>
2020-05-23 15:33:30 -0400

    (new feature) configuring connection with mongodb

--- gobarber/src/database/index.js
+++ gobarber/src/database/index.js
@@ -1,4 +1,6 @@
 import Sequelize from 'sequelize';
+import mongoose from 'mongoose'; 
+
 import dbConfig from '../config/database';
 
 import User from '../app/models/User';
@@ -10,12 +12,20 @@ const models = [User, File, Appointment];
 class Database {
   constructor() {
     this.init();
+    this.mongo();
   }
 
   init() {
     this.connection = new Sequelize(dbConfig);
     models.map((model) => model.init(this.connection))
     models.map((model) => model.associate && model.associate(this.connection.models));
+  }
+
+  mongo() {
+    this.mongoConnection = mongoose.connect(
+      'mongodb://localhost:27017/gobarber',
+      { useNewUrlParser: true, useFindAndModify: true, useUnifiedTopology: true }
+    )
   }
 }
 
