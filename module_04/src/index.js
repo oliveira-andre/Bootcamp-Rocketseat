32815b324771e13f22d7c0b327a7c2b3244a1bb1
Andre Oliveira <desmonplays@gmail.com>
2020-05-30 16:43:33 -0400

    (new feature) importing the app component

--- module_04/src/index.js
+++ module_04/src/index.js
@@ -1,3 +1,5 @@
-const sum = (a, b) => a + b;
+import React from 'react';
+import { render } from 'react-dom';
+import App from './App';
 
-alert(sum(1, 3));
+render(<App />, document.getElementById('app'));
