64671bc4e6757004878850a72db67e6d2482ca2d
Andre Oliveira <desmonplays@gmail.com>
2020-06-12 15:54:00 -0400

    (new feature) rendering routes with react router dom

--- module_05/src/App.js
+++ module_05/src/App.js
@@ -1,15 +1,9 @@
 import React from 'react';
-import logo from './logo.svg';
+import Routes from './routes';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
-    </div>
+    <Routes />
   );
 }
 
