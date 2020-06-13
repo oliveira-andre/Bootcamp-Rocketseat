572045bdcd6d0eebd98699f1f66ca49f67528ef4
Andre Oliveira <desmonplays@gmail.com>
2020-06-12 22:05:04 -0400

    (new feature) creating html body of form

--- module_05/src/pages/Main/index.js
+++ module_05/src/pages/Main/index.js
@@ -1,12 +1,22 @@
 import React from 'react';
+import { FaGithubAlt, FaPlus } from 'react-icons/fa';
 
-import { Title } from './styles';
+import { Container, Form, SubmitButton } from './styles';
 
 function Main() {
   return (
-    <Title>
-      Hello World
-    </Title>
+    <Container>
+      <h1>
+        <FaGithubAlt />
+        Repositórios
+      </h1>
+      <Form onSubmit={() => {}}>
+        <input type="text" placeholder="Adicionar Repositório" />
+        <SubmitButton>
+          <FaPlus color="#fff" size={14} />
+        </SubmitButton>
+      </Form>
+    </Container>
   );
 }
 
