3e01aaf981d34aef1113806d07e4d583c241e20b
Andre Oliveira <desmonplays@gmail.com>
2020-06-13 21:44:23 -0400

    (new feature) getting param repository name

--- module_05/src/pages/Repository/index.js
+++ module_05/src/pages/Repository/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
-function Repository() {
-  return <h1>repo</h1>;
+function Repository({ match }) {
+  return <h1>repo {decodeURIComponent(match.params.repository)}</h1>;
 }
 
 export default Repository;
