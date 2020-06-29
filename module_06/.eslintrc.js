a98dbd7523d4682835822edaac952c702289b5c3
Andre Oliveira <desmonplays@gmail.com>
2020-06-29 16:22:57 -0400

    feature: configuring the filenames extensions to js and jsx, and not preffer the import export default

--- module_06/.eslintrc.js
+++ module_06/.eslintrc.js
@@ -1,4 +1,33 @@
 module.exports = {
-  root: true,
-  extends: '@react-native-community',
+  "env": {
+    "es2020": true
+  },
+  "extends": [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "prettier",
+    "prettier/react"
+  ],
+  "parser": "babel-eslint"
+  "parserOptions": {
+    "ecmaFeatures": {
+      "jsx": true
+    },
+    "ecmaVersion": 11,
+    "sourceType": "module"
+  },
+  "plugins": [
+    "react",
+    "prettier",
+  ],
+  "rules": {
+    'prettier/prettier': 'error',
+    'react/jsx-filename-extension':[
+      'warn',
+      {
+        extensions: ['.jsx', '.js']
+      }
+    ],
+    'import/preffer-default-export': 'off'
+  },
 };
