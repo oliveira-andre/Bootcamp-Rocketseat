26aeb4d292d2c446dedd588896612175c8527f99
Andre Oliveira <desmonplays@gmail.com>
2020-06-30 20:20:13 -0400

    feature: cleaning index.js

--- module_06/src/index.js
+++ module_06/src/index.js
@@ -1,37 +1,12 @@
 import React from 'react';
-import {
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-} from 'react-native';
+import { View } from 'react-native';
 
 import './config/ReactotronConfig';
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-});
-
-console.tron.log('teste')
 
 export default function App() {
   return (
     <>
-      <ScrollView>
-        <View style={styles.container}>
-          <Text style={styles.welcome}>teste</Text>
-          <Text style={styles.welcome}>teste abaixo</Text>
-        </View>
-      </ScrollView>
+      <View />
     </>
   );
 };
