47771617b0d833d9b00025c43b0a493d9526eecf
Andre Oliveira <desmonplays@gmail.com>
2020-06-30 20:45:50 -0400

    feature: installing title on header

--- module_06/src/index.js
+++ module_06/src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
-import { View } from 'react-native';
+import 'react-native-gesture-handler';
 
 import './config/ReactotronConfig';
+import Routes from './routes';
 
 export default function App() {
-  return (
-    <>
-      <View />
-    </>
-  );
+  return <Routes />;
+};
+
+Main.navigationOptions = {
+  title: 'Users'
 };
 
