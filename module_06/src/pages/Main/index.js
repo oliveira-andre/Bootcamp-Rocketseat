50ecb79a8dfce5016729849850c15c386d7e0f8a
Andre Oliveira <desmonplays@gmail.com>
2020-07-02 20:59:10 -0400

    feature: catching the submit button sending to githubapi and inserting the user data

--- module_06/src/pages/Main/index.js
+++ module_06/src/pages/Main/index.js
@@ -1,23 +1,57 @@
-import React from 'react';
-import { Container } from './styles';
+import React, { Component } from 'react';
+import { Keyboard } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
-export default function Main() {
-  return (
-    <>
-      <Container>
-        <Form>
-          <Input
-            autoCorrect={ false }
-            autoCapitalize='none'
-            placeholder='Adicionar usuário'
-          />
-          <SubmitButton>
-            <Icon name='add' size={20} color='#FFF' />
-          </SubmitButton>
-        </Form>
-      </Container>
-    </>
-  );
+import api from '../../services/api';
+import { Container, Form, Input, SubmitButton } from './styles';
+
+export default class Main extends Component {
+  state = {
+    newUser: '',
+    users: [],
+  };
+
+  handleSubmit() = () => {
+    const { users, newUser } = this.state;
+    const response = await api.get(`/users/${newUser}`);
+    const data = {
+      name: response.data.name,
+      login: response.data.login,
+      bio: response.data.bio,
+      avatar: response.data.avatar,
+    };
+
+    this.setState({
+      users: [...users, data],
+      newUser: '',
+    });
+
+    Keyboard.dismiss();
+  }
+
+  render() {
+    const { users, newUSer } = this.state;
+
+    return (
+      <>
+        <Container>
+          <Form>
+            <Input
+              autoCorrect={ false }
+              autoCapitalize='none'
+              placeholder='Adicionar usuário'
+              value={newUser}
+              onChangeText={ text => setState({ newUser: text }) }
+              returnKeyType='send' 
+              onSubmitEditing={ this.handleSubmit }
+            />
+            <SubmitButton onPress={ this.handleSubmit }>
+              <Icon name='add' size={20} color='#FFF' />
+            </SubmitButton>
+          </Form>
+        </Container>
+      </>
+    );
+  };
 };
 
