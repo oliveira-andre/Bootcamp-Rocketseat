00e268867ace8c92c9759eb1797aa728b35d9bb4
Andre Oliveira <desmonplays@gmail.com>
2020-07-02 20:24:15 -0400

    feature: creating and styling, container, text input, form and submit button

--- module_06/src/pages/Main/index.js
+++ module_06/src/pages/Main/index.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import { Container } from './styles';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 export default function Main() {
   return (
     <>
-      <Container />
+      <Container>
+        <Form>
+          <Input
+            autoCorrect={ false }
+            autoCapitalize='none'
+            placeholder='Adicionar usuário'
+          />
+          <SubmitButton>
+            <Icon name='add' size={20} color='#FFF' />
+          </SubmitButton>
+        </Form>
+      </Container>
     </>
   );
 };
--- module_06/src/pages/Main/styles.js
+++ module_06/src/pages/Main/styles.js
@@ -1,7 +1,33 @@
 import styled from 'styled-components/native';
+import { RectButton } from 'react-native-gesture-handler';
 
 export const Container = styled.View`
   flex: 1;
   padding: 30px;
+`;
 
+export const Form = styled.View`
+  flex-direction: row;
+  padding-botton: 20px;
+  border-botton-width: 1px;
+  border-color: #eee;
+`;
+
+export const Input = styled.TextInput.attrs({
+  placeholderTextColor: '#999';
+})`
+  flex: 1;
+  height: 40px;
+  background: #eee;
+  border-radius: 4px;
+  padding: 0 15px;
+  border: 1px solid #eee;
+`;
+
+export const SubmitButton = styled(RectButton)`
+  justify-content: center;
+  align-items: center;
+  background: #751C1;
+  margin-left: 10px;
+  padding: 0 12px;
 `;
